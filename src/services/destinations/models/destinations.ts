3b4010ee52cfe73ecad13c8cd70b32970dda4671
Celitech <80042395+zgheibali@users.noreply.github.com>
2025-08-12 12:44:58 +0000

    liblab SDK update for version v1.3.50

--- src/services/destinations/models/destinations.ts
+++ src/services/destinations/models/destinations.ts
@@ -7,6 +7,7 @@ export const destinations = z.lazy(() => {
   return z.object({
     name: z.string().optional(),
     destination: z.string().optional(),
+    destinationIso2: z.string().optional(),
     supportedCountries: z.array(z.string()).optional(),
   });
 });
@@ -15,7 +16,8 @@ export const destinations = z.lazy(() => {
  *
  * @typedef  {Destinations} destinations
  * @property {string} - Name of the destination
- * @property {string} - ISO representation of the destination
+ * @property {string} - ISO3 representation of the destination
+ * @property {string} - ISO2 representation of the destination
  * @property {string[]} - This array indicates the geographical area covered by a specific destination. If the destination represents a single country, the array will include that country. However, if the destination represents a broader regional scope, the array will be populated with the names of the countries belonging to that region.
  */
 export type Destinations = z.infer<typeof destinations>;
@@ -29,11 +31,13 @@ export const destinationsResponse = z.lazy(() => {
     .object({
       name: z.string().optional(),
       destination: z.string().optional(),
+      destinationISO2: z.string().optional(),
       supportedCountries: z.array(z.string()).optional(),
     })
     .transform((data) => ({
       name: data['name'],
       destination: data['destination'],
+      destinationIso2: data['destinationISO2'],
       supportedCountries: data['supportedCountries'],
     }));
 });
@@ -47,11 +51,13 @@ export const destinationsRequest = z.lazy(() => {
     .object({
       name: z.string().optional(),
       destination: z.string().optional(),
+      destinationIso2: z.string().optional(),
       supportedCountries: z.array(z.string()).optional(),
     })
     .transform((data) => ({
       name: data['name'],
       destination: data['destination'],
+      destinationISO2: data['destinationIso2'],
       supportedCountries: data['supportedCountries'],
     }));
 });
